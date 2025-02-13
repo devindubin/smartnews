2939e581a31c551d77c0dede20ad2ea765e9af32
Devin <devindubin@gmail.com>
2025-02-13 14:21:09 -0500

    reploying as static and web service separately

--- backend/server.js
+++ backend/server.js
@@ -22,7 +22,7 @@ const PORT = process.env.PORT || 5000;
 connectDB();
 getArticlesOnSchedule();
 
-app.use(express.static(path.join(__dirname, "../frontend/dist")));
+// app.use(express.static(path.join(__dirname, "../frontend/dist")));
 
 app.use(credentials);
 app.use(
--- frontend/src/api/axios.jsx
+++ frontend/src/api/axios.jsx
@@ -1,6 +1,7 @@
 import axios from "axios";
 
-const BASE_URL = "https://smartnews-5q5f.onrender.com"; //|| "http://localhost:5000";
+const BASE_URL = "https://smartnews-5q5f.onrender.com";
+// const BASE_URL = "http://localhost:5000";
 
 export default axios.create({
   baseURL: BASE_URL,
