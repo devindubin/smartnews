68e9e32fafe2039b978dc1a0e91710163a7f72a6
Devin <devindubin@gmail.com>
2025-02-15 11:11:34 -0500

    added partitioned cookie;

--- backend/controllers/authControllers.js
+++ backend/controllers/authControllers.js
@@ -57,7 +57,7 @@ export const handleLogin = async (req, res) => {
     console.log(result);
     //creates secure cookie with refresh token
     res.cookie("jwt", refreshToken, {
-      domain: ".onrender.com",
+      partitioned: true,
       httpOnly: true,
       secure: true,
       sameSite: "None",
