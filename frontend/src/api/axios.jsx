6bba3fc13e4bf84b0b1fe8cd3c8b4446c8d9cbe7
Devin <devindubin@gmail.com>
2025-02-15 12:40:17 -0500

    updated base url to new api subdomain

--- frontend/src/api/axios.jsx
+++ frontend/src/api/axios.jsx
@@ -1,6 +1,7 @@
 import axios from "axios";
 
-const BASE_URL = "https://smartnews-backend.onrender.com";
+const BASE_URL = "https://www.api.smrtnews.org/";
+// const BASE_URL = "https://smartnews-backend.onrender.com";
 // const BASE_URL = "http://localhost:5000";
 
 export default axios.create({
